c3cb5ea6866e95b36b36e842ba6c1bf64917380d
mrkuc <yushi.uc@gmail.com>
2016-06-19 00:02:09 +0900

    makeBoard

--- Reversi.elm
+++ Reversi.elm
@@ -1,20 +1,46 @@
 module Reversi where
 
+import List exposing (..)
+
 -- 駒
-type Piece = Head | Tail
+type Piece = Black | White | None
 
--- Maybe Piece を Squareとして扱いたい
+type alias Pos = (Int, Int) -- 座標
+-- 1マス
+type alias Square = 
+    { piece: Piece
+    , pos: Pos
+    }
+type alias Board = List Square -- 盤
 
+-- 盤面
+w = 8
+h = 8
+board = combinationOf (\a -> \b -> {piece = a, pos = b}) [1..w] [1..h]
+
+-- くみあわせ
+combinationOf : (a -> b -> c) -> List a -> List b -> List c
+combinationOf f a b =
+    List.concat (List.map (\n -> List.map (\m -> (f n m)) b) a)
+
+-- 盤面生成
+makePos : Int -> Int -> Pos
+makePos n m =
+    (n, m)
+    
+makeBoard : Int -> Int -> Board
+makeBoard w h =
+    let
+        squares = 
+            combinationOf makePos [1..w] [1..h]
+    in
+        combinationOf (\s -> \p -> {piece=p, pos=s}) squares (List.repeat (List.length squares) None)
+      
 -- オセロを反転
 reverse : Piece -> Piece
 reverse p = 
     case p of
-        Head -> Tail
-        Tail -> Head
+        Black -> White
+        White -> Black
+        None -> None
 
--- マスに駒を置く
-put : Maybe Piece -> Piece -> Result String (Maybe Piece) 
-put m p =
-    case m of
-        Just _ -> Err "not empty square..."
-        Nothing -> Ok (Just p)
--- Tests.elm
+++ Tests.elm
@@ -10,8 +10,9 @@ all =
     suite "A Test Suite"
         [
          test "Addition" (assertEqual (3 + 7) 10)
-        ,test "Reverse Head" (assertEqual Head (reverse Tail))
-        ,test "Reverse Tail" (assertEqual Tail (reverse Head))
-        ,test "Put Piece 1" (assertEqual (Ok (Just Head)) (put Nothing Head))
-        ,test "Put Piece 2" (assertEqual Nothing (Result.withDefault Nothing (put (Just Tail) Head)))
+        ,test "Reverse Black" (assertEqual White (reverse Black))
+        ,test "Reverse White" (assertEqual Black (reverse White))
+        ,test "Reverse None" (assertEqual None (reverse None))
+        ,test "combinationOf 1" (assertEqual 64 (List.length (combinationOf (\a -> \b -> {piece = a, pos = b}) [1..8] [1..8])))
+        ,test "makeBoard" (assertEqual {piece=None, pos=(1,1)} (Maybe.withDefault {piece=None, pos=(0,0)}(List.head (makeBoard 8 8))))
         ]
